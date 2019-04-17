07b620f326918579e9cba00d43babc7878678092
Victor Hugo <victorq32@gmail.com>
2019-04-17 18:45:10 -0300

    add fila de pai

--- src/distanciaEntreDistribuidores/Graph.java
+++ src/distanciaEntreDistribuidores/Graph.java
@@ -9,6 +9,8 @@ class Graph
 	private int V; // No. of vertices 
 	private LinkedList<Integer> adj[]; //Adjacency Lists 
 	private int pai[];
+	private int cont =0;
+	private int vet[];
 
 	Graph(int v) 
 	{ 
@@ -28,31 +30,46 @@ class Graph
 			System.out.printf(" %d",pai[i]);
 		}
 	}
+	
+	void imprimirVet() {
+		for(int i = 0 ; i < vet.length; i++) {
+			System.out.printf(" %d",vet[i]);
+		}
+	}
 
 	void BFS(int s) 
 	{ 
 		boolean visited[] = new boolean[V]; 
 		pai = new int[V];
+		vet = new int[V];
 		
 		LinkedList<Integer> queue = new LinkedList<Integer>(); 
 
 		visited[s]=true;
-		//pai[s] = s;
+		vet[s] = cont++;
+		pai[s] = s;
 		queue.add(s); 
+		//vet[s] = vet[s] + 1;
 
 		while (queue.size() != 0) 
 		{ 
 			s = queue.poll(); 
 			System.out.print(s+" "); 
+			
+			//vet[s] = vet[s] + 1;
 
 			Iterator<Integer> i = adj[s].listIterator(); 
 			while (i.hasNext()) 
 			{ 
-				int n = i.next(); 
+				
+				//cont = 0;
+				int n = i.next();
+				//vet[n] = cont++;
 				if (!visited[n]) 
 				{ 
 					visited[n] = true;
-					pai[n] = 9;
+					vet[n] = cont++;
+					pai[n] = s;
 					queue.add(n); 
 				} 
 			} 
@@ -61,21 +78,35 @@ class Graph
 
 	public static void main(String args[]) 
 	{ 
-		Graph g = new Graph(4); 
+		Graph g = new Graph(10); 
 
-		g.addEdge(0, 1); 
-		g.addEdge(0, 2); 
-		g.addEdge(1, 2); 
-		g.addEdge(2, 0); 
-		g.addEdge(2, 3); 
-		g.addEdge(3, 3); 
+		/*
+		 * g.addEdge(0, 2); g.addEdge(0, 3); g.addEdge(0, 4); g.addEdge(1, 2);
+		 * g.addEdge(1, 4); g.addEdge(2, 4); g.addEdge(3, 4); g.addEdge(3, 5);
+		 * g.addEdge(4, 5); g.addEdge(5, 1);
+		 */
+		
+		g.addEdge(9,0); 
+		g.addEdge(1,1);
+		g.addEdge(4,2); 
+		g.addEdge(9,3); 
+		g.addEdge(0,4); 
+		g.addEdge(4,5); 
+		g.addEdge(8,6); 
+		g.addEdge(9,7); 
+		g.addEdge(0,8); 
+		g.addEdge(1,9);
+		
+	
 
 		System.out.println("Following is Breadth First Traversal "+ 
 						"(starting from vertex 2)"); 
 
-		g.BFS(2); 
+		g.BFS(1); 
 		System.out.println(); 
 		g.imprimirPai();
+		System.out.println(); 
+		g.imprimirVet();
 	} 
 } 
 
